bbc968aeb346f87bff58a6ed6efbe72bdd1f9271
Hidayah Jalil <jinggeol00@gmail.com>
2022-12-15 02:34:17 +0800

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,4 @@
-import {
-  Box,
-  Button,
-  position,
-  TextField,
-  Input,
-  Grid,
-  Fragment,
-  // makeStyles
-} from '@mui/material'
-import { makeStyles } from '@mui/styles'
+import {TextField} from '@mui/material'
 //import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 import Geocode from "react-geocode";
 import {
@@ -26,7 +16,7 @@ var center = { lat: 4.8584, lng: 102.2945 }                           //default 
 
 function App(props) {
 
-  const [google] = useState(props);
+  const [google] = useState();
 
   const [marker, setMarker] = useState()
 
@@ -63,11 +53,11 @@ function App(props) {
 
   }
 
-  function clearRoute() {
-    setDirectionsResponse(null)
-    document.getElementById('location').value = '';
-    // originRef.current.value = ''
-  }
+  // function clearRoute() {
+  //   setDirectionsResponse(null)
+  //   document.getElementById('location').value = '';
+  //   // originRef.current.value = ''
+  // }
 
   return (
 
@@ -82,7 +72,7 @@ function App(props) {
               placeholder={''}
               type='text'
               ref={originRef}
-              onKeyPress={getPlace}
+              // onKeyPress={getPlace}
               style={{ width: '80rem' }}
             />
           </Autocomplete>
@@ -90,49 +80,8 @@ function App(props) {
         <div className='col-lg-4'>
           <img src={Search} className='schicon' onClick={() => getPlace()} />
         </div>
-        {/* </item> */}
-        {/* </Grid> */}
       </div>
-      {/* <Box
-        sx={{
-          display: 'flex',
-          position: 'relative',
-          flexDirection: 'row',
-          alignItems: 'center',
-          height: '50%',
-          width: '100%',
-          justifyContent: 'center',
-          // backgroundColor: 'beige',
-          zIndex: '99999',
-          //gap: '10px'
 
-        }}>
-        <div className='wrapper'>
-          <Autocomplete>
-            <TextField id="location" variant='filled' label={'Enter a location'} placeholder={''} type='text' ref={originRef} onKeyPress={getPlace} />
-          </Autocomplete> */}
-      {/* <Button variant='contained' type='submit' onClick={getPlace} >Find</Button> */}
-      {/* <div>
-        <img src={Search} className='schicon' onClick={() => getPlace()} />
-      </div>
-      <Button variant='contained' type='submit' onClick={() => clearRoute()} >X</Button>
-    </div>
-      </Box > */}
-      {/* 
-      <Grid container lg={12}>
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '0',
-            left: '0',
-            height: '100%',
-            width: '100%'
-            //backgroundColor: 'beige'
-
-          }}> */}
-
-      {/* <Grid item lg={12}>
-        <item> */}
 
       <div className='map'>
         <GoogleMap
@@ -150,7 +99,7 @@ function App(props) {
           onLoad={map => setMap(map)}
         >
           <Marker position={marker} />
-          {console.log(marker, "getThisMarker")}
+          {/* {console.log(marker, "getThisMarker")} */}
           {directionsResponse && (
             <DirectionsRenderer directions={directionsResponse} />
           )}
@@ -159,64 +108,6 @@ function App(props) {
 
     </React.Fragment>
 
-
-    // <Flex
-    //   position='relative'
-    //   flexDirection='column'
-    //   alignItems='center'
-    //   h='100vh'
-    //   w='100vw'
-    // >
-
-    //   <Box position='absolute' left={0} top={0} h='100%' w='100%'>
-    //     {/* Google Map Box */}
-    //     <GoogleMap
-    //       center={center}
-    //       zoom={15}
-    //       mapContainerStyle={{ width: '100%', height: '100%' }}
-    //       options={{
-    //         zoomControl: false,
-    //         streetViewControl: false,
-    //         mapTypeControl: false,
-    //         fullscreenControl: false,
-    //       }}
-    //       onLoad={map => setMap(map)}
-    //     >
-    //       <Marker position={center} />
-    //       {directionsResponse && (
-    //         <DirectionsRenderer directions={directionsResponse} />
-    //       )}
-    //     </GoogleMap>
-    //   </Box>
-    //   <Box
-    //     p={4}
-    //     borderRadius='lg'
-    //     m={4}
-    //     bgColor='white'
-    //     shadow='base'
-    //     minW='container.md'
-    //     zIndex='1'
-    //   >
-    //     <HStack spacing={2} justifyContent='space-between'>
-    //       <Box flexGrow={1}>
-    //         <Autocomplete>
-    //           <Input type='text' placeholder='Origin' ref={originRef} />
-    //         </Autocomplete>
-    //       </Box>
-
-    //       <ButtonGroup>
-    //         <Button colorScheme='pink' type='submit' onClick={getPlace} >
-    //           Place
-    //         </Button>
-    //         <IconButton
-    //           aria-label='center back'
-    //           icon={<FaTimes />}
-    //           onClick={clearRoute}
-    //         />
-    //       </ButtonGroup>
-    //     </HStack>
-    //   </Box>
-    // </Flex>
   )
 }
 
